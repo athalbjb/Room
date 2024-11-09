536b05b949b4cf60e85eedc60d39258100b04861
xHacker <115514882+athalbjb@users.noreply.github.com>
2024-11-08 20:38:42 -0800

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal md:mb-8 lg:mb-0 md:m-8 lg:mt-0 lg:m-0">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal md:m-8 lg:m-0">
             Built with ❤️ by Ankush Shah
           </h2>
           <CurrentTime /> {/* Use the CurrentTime component here */}
